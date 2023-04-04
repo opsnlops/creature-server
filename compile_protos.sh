40836cc2a564bd1e18b66ad6cf5fabec7cd5cb8c
April White <april@opsnlops.io>
2023-04-03 17:14:16 -0700

    Make Swift files, too

--- compile_protos.sh
+++ compile_protos.sh
@@ -3,5 +3,10 @@
 export SRC_DIR=messaging
 export DST_DIR=src/messaging
 
+# CPP
 protoc -I=$SRC_DIR --cpp_out=$DST_DIR $SRC_DIR/server.proto
 protoc -I=$SRC_DIR --grpc_out=$DST_DIR --plugin=protoc-gen-grpc=$(which grpc_cpp_plugin) $SRC_DIR/server.proto
+
+# Swift
+protoc --swift_out=swift/ $SRC_DIR/server.proto
+protoc --grpc-swift_out=swift/ --grpc-swift_opt=Client=true,Server=false $SRC_DIR/server.proto
