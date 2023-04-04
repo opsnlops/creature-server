eb8fcd661529949dd21414c6b965190929363d70
April White <april@opsnlops.io>
2023-04-03 18:59:48 -0700

    Make the Swift clients `public` in scope

--- compile_protos.sh
+++ compile_protos.sh
@@ -8,5 +8,5 @@ protoc -I=$SRC_DIR --cpp_out=$DST_DIR $SRC_DIR/server.proto
 protoc -I=$SRC_DIR --grpc_out=$DST_DIR --plugin=protoc-gen-grpc=$(which grpc_cpp_plugin) $SRC_DIR/server.proto
 
 # Swift
-protoc --swift_out=swift/ $SRC_DIR/server.proto
-protoc --grpc-swift_out=swift/ --grpc-swift_opt=Client=true,Server=false $SRC_DIR/server.proto
+protoc --swift_out=swift/ --swift_opt=Visibility=Public $SRC_DIR/server.proto
+protoc --grpc-swift_out=swift/ --grpc-swift_opt=Visibility=Public --grpc-swift_opt=Client=true,Server=false $SRC_DIR/server.proto
