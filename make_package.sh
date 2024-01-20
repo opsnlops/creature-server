5a0ee982b1b6c571332f403d26794c8e8c98579a
April White <april@opsnlops.io>
2024-01-20 00:22:30 -0800

    Add a way to make a package locally

---
+++ make_package.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#
+# Build an image the same way the GHA build does
+#
+
+docker build -f Dockerfile.package . -t temp-image
+docker create --name temp-container temp-image
+mkdir -p package/ && docker cp temp-container:/package/ .
+docker rm temp-container
+
+ls -lart package/
